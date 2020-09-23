ba16d48c7f4ec13ffbf8045966107280d11101c2
Andrea <miranda.andrea.nd@gmail.com>
2020-09-23 18:09:57 +0200

    finito

--- index.php
+++ index.php
@@ -1,37 +1,56 @@
 <?php
 
-class Square {
-  public $lato;
+  class Square {
+    public $lato;
 
-  // CONSTRUCT
-  public function __construct($lato) {
-    $this -> lato = $lato;
+    // CONSTRUCT
+    public function __construct($lato) {
+      $this -> lato = $lato;
+    }
+    // CALCOLO DELL'AREA DEL QUADRATO
+    public function getArea() {
+      return $this -> lato * $this -> lato ;
+    }
+    // CALCOLO DEL PERIMETRO DEL QUADRATO
+    public function getPerimeter() {
+      return $this -> lato * 4;
+    }
+    // STAMPA
+    public function __toString() {
+      return "Quadrato:" . '<br>'
+      . "Area: " . $this -> getArea() . '<br>'
+      . "Perimetro: " . $this -> getPerimeter() . '<br>' . '<br>';
+    }
   }
 
-  // CALCOLO DELL'AREA DEL QUADRATO
-  public function getArea() {
-    return $this -> lato * $this -> lato ;
+  class Cube extends Square {
+
+    // CALCOLO VOLUME DEL CUBO
+    public function getVolumes() {
+      return $this -> lato * $this -> lato * $this -> lato;
+    }
+    // CALCOLO DELLA SUPERFICIE DEL CUBO
+    public function getSurface() {
+      return 6 * $this -> lato * $this -> lato;
+    }
+    // STAMPA
+    public function __toString() {
+      return "Cubo: " . '<br>'
+      . "Volume: " . $this -> getVolumes() . '<br>'
+      . "Superficie: " . $this -> getSurface() . '<br>';
+    }
   }
 
-  // CALCOLO DEL PERIMETRO DEL QUADRATO
-  public function getPerimeter() {
-    return $this -> lato * 4;
 
-  }
+  $quadrato_1 = new Square (23);
+  echo $quadrato_1;
 
-  public function __toString() {
-    return "Quadrato:" . '<br>'
-         . "Area: " . $this -> getArea() . '<br>'
-         . "Perimetro: " . $this -> getPerimeter() . '<br>';
-  }
-
-}
+  $cubo = new Cube (15);
+  echo $cubo;
 
 
-class Cube {
 
-}
 
-$quadrato_1 = new Square (10);
-echo $quadrato_1;
- ?>
+
+
+?>
